9df46ec869df09ac2a2168892203e53eab2e0311
DiogoS2099 <diogo.miguel.c.santos@gmail.com>
2022-10-30 17:07:17 +0000

    Switched to Fastify for performance. Added dotenv to be able to configure environment variables

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+
+interface Config {
+  port: number;
+}
+
+dotenv.config();
+
+export const config: Config = {
+  port: parseInt(process.env.HOST_PORT!, 10),
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from './config';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(config.port, '0.0.0.0');
 }
 bootstrap();
