c7fd8829bf68251061a93190e0e7895ef4972376
DiogoS2099 <diogo.miguel.c.santos@gmail.com>
2022-10-30 19:18:49 +0000

    Added swagger, because yes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller()
+@ApiTags('Example API')
+@Controller('example')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    summary: 'Asynchronously fetch an object.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: String,
+  })
   @Get()
   async get(): Promise<string> {
     return await this.appService.get();
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { config } from './config';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('nestjs-template')
+    .setDescription('NestJS template description')
+    .setVersion('1.0')
+    .addTag('example')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(config.port, '0.0.0.0');
 }
 bootstrap();
